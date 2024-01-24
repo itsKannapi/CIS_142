17c3d0f043e213659127d3ea1340c98fb8a19bb5
itsKannapi <wubzei38@gmail.com>
2024-01-24 14:23:33 -0600

    upgraded simple code

--- FirstAppSpring2024.cpp
+++ FirstAppSpring2024.cpp
@@ -2,20 +2,30 @@
 
 #include <iostream>
 using namespace std;
+
+double CalcPayment(double, int);
+
 int main(){
+	//Declaring Variables
+	double PayRate = 15.50;
+	int HoursWorked = 40;
+	double Payment;
+	cout << "Payroll System" << endl;
+	cout << "==================\n\n";
+	cout << "Enter the Pray Rate ==> ";
+	cin >> PayRate;
+	cout << "Enter the hours you have worked ==> ";
+	cin >> HoursWorked;
+	Payment = CalcPayment(HoursWorked, PayRate);
 
-    std::cout << "This is Joey!\n";
-
-    
+	cout << "Your Payment is ==> " << Payment << "\n";+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+double CalcPayment(double h, int p) {
+	if (h <= 40) {
+		return p * h;
+	}
+	else {
+		return (40 * p) + ((h - 40) * (p * 1.5));
+	}
+}
